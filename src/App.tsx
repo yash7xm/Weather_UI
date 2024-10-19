4a42807feabe8734120f9ed516218beac230d409
yash7xm <yash.7xm@gmail.com>
2024-10-20 00:34:46 +0530

    weather card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "./components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+import { Weather } from "./components/weather";
 
-export default function App({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-  )
+export default function App() {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                <Weather />
+            </main>
+        </SidebarProvider>
+    );
 }
---
+++ src/components/weather/index.tsx
@@ -0,0 +1,9 @@
+import { WeatherCard } from "./weather-card";
+
+export function Weather() {
+    return (
+        <div className="">
+            <WeatherCard />
+        </div>
+    );
+}
