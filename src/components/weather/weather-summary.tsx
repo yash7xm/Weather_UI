7afce6bbe6e7def01de46781efeff613ce64cd79
yash7xm <yash.7xm@gmail.com>
2024-10-20 13:33:55 +0530

    calendar added

--- src/components/weather/weather-summary.tsx
+++ src/components/weather/weather-summary.tsx
@@ -1,7 +1,5 @@
-import { CheckIcon } from "@radix-ui/react-icons";
-
+import { Calendar1Icon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -11,18 +9,27 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
 const notifications = [
     {
         title: "Min temp of the day",
-        description: "22 C",
+        description: "22 °C",
     },
     {
         title: "Max temp of the day",
-        description: "25 C",
+        description: "25 °C",
     },
     {
         title: "Average temp of the day",
-        description: "23 C",
+        description: "23 °C",
     },
     {
         title: "Dominant weather condition",
@@ -33,14 +40,18 @@ const notifications = [
 type CardProps = React.ComponentProps<typeof Card>;
 
 export function WeatherSummary({ className, ...props }: CardProps) {
+    const [date, setDate] = useState<Date | undefined>(new Date());
+
+    const formattedDate = date
+        ? `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+        : "Select a date";
+
     return (
         <div className="absolute left-[60%]">
             <Card className={cn("w-[380px]", className)} {...props}>
                 <CardHeader>
                     <CardTitle>Summary</CardTitle>
-                    <CardDescription>
-                        Today's Weather Summary
-                    </CardDescription>
+                    <CardDescription>Today's Weather Summary</CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-4">
                     <div>
@@ -63,9 +74,23 @@ export function WeatherSummary({ className, ...props }: CardProps) {
                     </div>
                 </CardContent>
                 <CardFooter>
-                    <Button className="w-full">
-                        <CheckIcon /> Mark all as read
-                    </Button>
+                    <div className="w-full flex justify-between items-center">
+                        <div>Pick a date</div>
+                        <div>{formattedDate}</div>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Calendar1Icon className="cursor-pointer" />
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    onSelect={setDate}
+                                    className="rounded-md border"
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </div>
                 </CardFooter>
             </Card>
         </div>
